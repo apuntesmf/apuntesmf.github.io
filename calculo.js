9647a9331886d7c792a42d3977a4d8ab46427e06
apuntesmf <86994815+apuntesmf@users.noreply.github.com>
2023-05-12 00:49:22 -0700

    actualizacion
    se agrega area para realizar las notas medicas

---
+++ calculo.js
@@ -0,0 +1,300 @@
+function calc(form){
+    var A = document.getElementById("kg").value;
+    var B = document.getElementById("ml").value;
+    var C = document.getElementById("hrs").value;
+    var D = "0";
+
+    if (document.getElementById("m2SC").checked){
+        if (A < 10){
+            var D = (((parseInt(A) * 4)) + 9) / 100;
+            var D = (D * B) / C;
+            document.getElementById("resultado").innerHTML = D;
+            return false;
+        }
+        else{
+            var F = (((parseInt(A) * 4)) + 7) / (parseInt(A) + 90);
+            var F = F * B
+            var D = F / C
+            var D = D.toFixed(2)
+            document.getElementById("resultado").innerHTML = D + " ml";
+            return false;
+        }
+
+    }
+    else{
+        if (A <= 10){
+            var D = A *  100
+            var D = D / C
+            document.getElementById("resultado").innerHTML = D + " ml";
+            return false;
+        }
+        else if ( A >= 11 && A <= 20){
+            var E = A - 10
+            var E = E *  50
+            var D = E + 1000
+            var D = D / C
+            document.getElementById("resultado").innerHTML = D + " ml";
+            return false;
+        }
+        else{
+            var E = A - 20
+            var E = E * 20
+            var D = E + 1500
+            var D = D / C
+            document.getElementById("resultado").innerHTML = D + " ml";
+            return false;
+        }
+
+
+    }
+
+}
+function calc2(form){
+    var A = document.getElementById("kg").value;
+    var B = document.getElementById("mg").value;
+    var C = document.getElementById("ml").value;
+    var D = document.getElementById("dosis").value;
+    var E = document.getElementById("hrs").value;
+    var F = "0";
+
+    if (document.getElementById("dia").checked){
+        var F = (((A * D) * (C) / B)/E);
+        var F = F.toFixed(2)
+        document.getElementById("resultado").innerHTML = F + " ml.";
+        return false;
+    }
+    else{
+        var F = (((A * D) * C) / B);
+        var F = F.toFixed(2)
+        document.getElementById("resultado").innerHTML = F + " ml.";
+        return false;
+    }
+
+}
+
+function emb(form){
+    var sdg = document.getElementById("sdg").value;
+    if (sdg <14){
+        const element = document.getElementById('selector');
+        element.style.backgroundColor = 'red';
+    }
+    /*  var dia = document.getElementById("dia").value;
+    var mes = document.getElementById("mes").value;
+    var ano = document.getElementById("ano").value;
+
+    mese = mes - 3;
+    dias = parseInt(dia) + 7;
+
+    var fd = new Date();
+    ano = fd.getFullYear();
+    dia = fd.getDate();
+    mes = fd.getMonth();
+    document.getElementById("resultado").innerHTML = dia + " / " + mes + " / " + ano;
+    return false;
+}
+
+
+
+    switch(mes){
+        case 0:
+            mes = 12;
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+        case 1:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+
+        case 2:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+
+        case 3:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa = mes + 1 ;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa;
+                return false;
+            }
+
+        case 4:
+            ano = parseInt(ano) + 1
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes + " / " + ano;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 5:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 6:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 7:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 8:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 9:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case -2:
+            var mesa = 10;
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                mesa = mesa + 1;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case -1:
+            var mesa = 11;
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                mesa = mesa + 1;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+
+        case 12:
+            if(dia <=31){
+                document.getElementById("resultado").innerHTML = dia + " / " + mes;
+                return false;
+            }
+            else{
+                dia = dia - 31;
+                var mesa =  mesa + 1;
+                document.getElementById("resultado").innerHTML = dia + " / " + mesa + " / " + ano;
+                return false;
+            }
+    }*/
+}
+function selec(form){
+    var sdg = document.getElementById("sdg").value;
+    if (sdg <14){
+        const element = document.getElementById('selector');
+        element.style.backgroundColor = 'green';
+    }
+    else if( sdg >=14 && sdg <=24){
+        const elemen2 = document.getElementById('selector2');
+        element2.style.backgroundColor = 'green';
+        const element = document.getElementById('selector');
+        element.style.backgroundColor = '#dddddd';
+    }
+    else if( sdg >24 && sdg <=28){
+        const element3 = document.getElementById('selector3');
+        element3.style.backgroundColor = '#green';
+        const element2 = document.getElementById('selector');
+        element2.style.backgroundColor = '#dddddd';
+        const element = document.getElementById('selector2');
+        element.style.backgroundColor = '#dddddd';
+    }
+    else if( sdg >=28 && sdg <=34){
+        const element4 = document.getElementById('selector4');
+        element4.style.backgroundColor = 'green';
+        const element2 = document.getElementById('selector2');
+        element2.style.backgroundColor = '#dddddd';
+        const element3 = document.getElementById('selector3');
+        element3.style.backgroundColor = '#dddddd';
+        const element = document.getElementById('selector');
+        element.style.backgroundColor = '#dddddd';
+    }
+    else if( sdg >34 && sdg <=40){
+      const element5 = document.getElementById('selector5');
+      element5.style.backgroundColor = 'green';
+      const element2 = document.getElementById('selector2');
+      element2.style.backgroundColor = '#dddddd';
+      const element3 = document.getElementById('selector3');
+      element3.style.backgroundColor = '#dddddd';
+      const element4 = document.getElementById('selector4');
+      element4.style.backgroundColor = '#dddddd';
+      const element = document.getElementById('selector');
+      element.style.backgroundColor = '#dddddd';
+    }
+}
---
+++ gnota.js
@@ -0,0 +1,13 @@
+function nota(form){
+    var nombre1 = document.getElementById("nombre").value+ document.getElementById("apellido-paterno").value+ document.getElementById("apellido-materno").value;
+    var app = "\n\n App: " + document.getElementById("alergia").value + document.getElementById("enfermedades").value + document.getElementById("cirugias").value  + document.getElementById("hospitalizaciones").value  + document.getElementById("traumatismo").value + document.getElementById("transfusion").value;  
+    var apnp = "\n\n Apnp: " + document.getElementById("etilismo").value + document.getElementById("tabaquismo").value + document.getElementById("toxico").value ;  
+    var pa = "\n\n Pa:" + document.getElementById("pa").value;
+    var ef="\n\n Ef:" + document.getElementById("neuro").value + document.getElementById("piel").value + document.getElementById("cabeza").value + document.getElementById("cuello").value + document.getElementById("torax").value + document.getElementById("abdomen").value + document.getElementById("genitales").value + document.getElementById("extremidad").value;
+    var analisis="\n\n Analisis:"+ document.getElementById("analisis").value;
+    var dx="\n\n Diagnostico:"+ document.getElementById("analisis").value;
+    var plan="\n\n Plan:"+ document.getElementById("analisis").value;
+    var text= document.getElementById("final");
+    
+    text.value = nombre1 + app + apnp + pa + ef + analisis+dx+plan;
+}
