d150690bf54403741098a59e77cd254adf39f906
apuntesmf <86994815+apuntesmf@users.noreply.github.com>
2023-05-14 22:01:20 -0700

    actualizacion

--- src/scripts/gnota.js
+++ src/scripts/gnota.js
@@ -1,16 +1,24 @@
 
+function gyo(){
+    if(document.getElementById("sexo").value == "femenino"){
+        const selec = document.querySelector('.container-gyo');
+        const div = document.createElement('div');
+        div.classList('id','thediv');
+    }
+    
+}
+
 function nota(form){
     var nombre1 = document.getElementById("nombre").value+ document.getElementById("apellido-paterno").value+ document.getElementById("apellido-materno").value;
     var dato = "\n\n" + document.getElementById("edad").value +" \n"+ document.getElementById("sexo").value + " \n" +document.getElementById("peso").value + "kg";
-    var app = "\n\n App: " + document.getElementById("alergia").value + document.getElementById("enfermedades").value + document.getElementById("cirugias").value  + document.getElementById("hospitalizaciones").value  + document.getElementById("traumatismo").value + document.getElementById("transfusion").value;  
-    var apnp = "\n\n Apnp: " + document.getElementById("etilismo").value + document.getElementById("tabaquismo").value + document.getElementById("toxico").value ;  
-    var puta = "\n\n Gyo: " + "Menarca:" + " " + document.getElementById("menarca").value + " " + "Ivsa:" + document.getElementById("ivsa").value + " " + "Npsa:" + document.getElementById("npsa").value + " " +"Gestas:" + document.getElementById("gesta").value + " " + "Partos:" + document.getElementById("parto").value + " " + "Cesareas:" + document.getElementById("cesarea").value + " " + "Aborto:"+ document.getElementById("aborto").value + " " + "Citologia:" + document.getElementById("citologia").value + " " + "Fum:" + document.getElementById("fum").value;
-    var pa = "\n\n Pa:" + document.getElementById("pa").value;
-    var ef="\n\n Ef:" + document.getElementById("neuro").value + document.getElementById("piel").value + document.getElementById("cabeza").value + document.getElementById("cuello").value + document.getElementById("torax").value + document.getElementById("abdomen").value + document.getElementById("genitales").value + document.getElementById("extremidad").value;
-    var analisis="\n\n Analisis:"+ document.getElementById("analisis").value;
-    var dx="\n\n Diagnostico:"+ document.getElementById("idx").value;
-    var med ="";
-    var plan="\n\n Plan:"+ document.getElementById("plan").value+"\n";
+    var app = "\n\n App:\n " + document.getElementById("alergia").value + document.getElementById("enfermedades").value + document.getElementById("cirugias").value  + document.getElementById("hospitalizaciones").value  + document.getElementById("traumatismo").value + document.getElementById("transfusion").value;  
+    var apnp = "\n\n Apnp:\n " + document.getElementById("etilismo").value + document.getElementById("tabaquismo").value + document.getElementById("toxico").value ;  
+    var puta = "\n\n Gyo: \n " + "Menarca:" + " " + document.getElementById("menarca").value + " " + "Ivsa:" + document.getElementById("ivsa").value + " " + "Npsa:" + document.getElementById("npsa").value + " " +"Gestas:" + document.getElementById("gesta").value + " " + "Partos:" + document.getElementById("parto").value + " " + "Cesareas:" + document.getElementById("cesarea").value + " " + "Aborto:"+ document.getElementById("aborto").value + " " + "Citologia:" + document.getElementById("citologia").value + " " + "Fum:" + document.getElementById("fum").value;
+    var pa = "\n\n Pa:\n " + document.getElementById("pa").value;
+    var ef="\n\n Ef:\n " + document.getElementById("neuro").value + document.getElementById("piel").value + document.getElementById("cabeza").value + document.getElementById("cuello").value + document.getElementById("torax").value + document.getElementById("abdomen").value + document.getElementById("genitales").value + document.getElementById("extremidad").value;
+    var analisis="\n\n Analisis:\n "+ document.getElementById("analisis").value;
+    var dx="\n\n Diagnostico:\n "+ document.getElementById("idx").value;
+    var plan="\n\n Plan:\n "+ document.getElementById("plan").value+"\n";
     var text= document.getElementById("final");
     
     
@@ -24,13 +32,13 @@ function nota(form){
 function indicaciones(form){
 
     if (document.getElementById("alarma").value == "Deshidratacion"){
-        var text= document.getElementById("final");
-        text.value += "Medidas generales:\n"+"Datos de alarma(llanto sin lagrimas, hundimiento de ojos,irritabilidad, boca y lengua seca, llanto sin lagrimas, no presenta micciones)."+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia."; 
+        var text= document.getElementById("plan");
+        text.value += "Medidas generales:\n"+"Datos de alarma(llanto sin lagrimas, hundimiento de ojos,irritabilidad, boca y lengua seca, no presenta micciones)."+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia."; 
     }else if(document.getElementById("alarma").value == "Respiratoria"){
-        var text= document.getElementById("final");
+        var text= document.getElementById("plan");
         text.value += "Medidas generales:\n"+"Datos de alarma(disnea, sibnilancias, piel que se unde entre costillas, cambio de coloracion de unas y labios). "+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia.";
     }else if(document.getElementById("alarma").value == "Obstetrica"){
-        var text= document.getElementById("final");
+        var text= document.getElementById("plan");
         text.value += "Medidas generales:\n"+"Datos de alarma(Sangrado vaginal,  convulsiones, falta de motilidad fetal, fiebre, dolor de cabeza, ver luces, escuchar zumbidos, dolor abdominal.) "+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia.";
     }
 }
@@ -100,15 +108,15 @@ function calc2(form){
         var F = (((A * D) * (C) / B)/E);
         var F = F.toFixed(2)
         var L = medicamento +" "+ F + " ml  cada "+ E +"hrs por "+document.getElementById("dias").value+" dias";
-        var text= document.getElementById("final");
+        var text= document.getElementById("plan");
         text.value += L+"\n";
         return false;
     }
     else{
         var F = (((A * D) * C) / B);
         var F = F.toFixed(2)
-        var L = medicamento + F + " ml  cada "+ E +"hrs por "+document.getElementById("dias").value+" dias";
-        var text= document.getElementById("final");
+        var L = medicamento +" "+B+"/"+C+ " " + F + " ml  cada "+ E +"hrs por "+document.getElementById("dias").value+" dias";
+        var text= document.getElementById("plan");
         text.value += L+"\n";
         return false;
     }
