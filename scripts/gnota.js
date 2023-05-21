c50e7d8c8440cec3bc96a4ffd2d0d96c4da526d0
apuntesmf <86994815+apuntesmf@users.noreply.github.com>
2023-05-20 19:19:58 -0700

    Update gnota.js
    ss

--- scripts/gnota.js
+++ scripts/gnota.js
@@ -1,20 +1,29 @@
 
+function gyo(){
+    if(document.getElementById("sexo").value == "femenino"){
+        const selec = document.querySelector('.container-gyo');
+        const div = document.createElement('div');
+        div.classList('id','thediv');
+    }
+    
+}
+
 function nota(form){
     var nombre1 = document.getElementById("nombre").value+ document.getElementById("apellido-paterno").value+ document.getElementById("apellido-materno").value;
     var dato = "\n\n" + document.getElementById("edad").value +" \n"+ document.getElementById("sexo").value + " \n" +document.getElementById("peso").value + "kg";
-    var app = "\n\n App: " + document.getElementById("alergia").value + document.getElementById("enfermedades").value + document.getElementById("cirugias").value  + document.getElementById("hospitalizaciones").value  + document.getElementById("traumatismo").value + document.getElementById("transfusion").value;  
-    var apnp = "\n\n Apnp: " + document.getElementById("etilismo").value + document.getElementById("tabaquismo").value + document.getElementById("toxico").value ;  
-    var puta = "\n\n Gyo: " + "Menarca:" + " " + document.getElementById("menarca").value + " " + "Ivsa:" + document.getElementById("ivsa").value + " " + "Npsa:" + document.getElementById("npsa").value + " " +"Gestas:" + document.getElementById("gesta").value + " " + "Partos:" + document.getElementById("parto").value + " " + "Cesareas:" + document.getElementById("cesarea").value + " " + "Aborto:"+ document.getElementById("aborto").value + " " + "Citologia:" + document.getElementById("citologia").value + " " + "Fum:" + document.getElementById("fum").value;
-    var pa = "\n\n Pa:" + document.getElementById("pa").value;
-    var ef="\n\n Ef:" + document.getElementById("neuro").value + document.getElementById("piel").value + document.getElementById("cabeza").value + document.getElementById("cuello").value + document.getElementById("torax").value + document.getElementById("abdomen").value + document.getElementById("genitales").value + document.getElementById("extremidad").value;
-    var analisis="\n\n Analisis:"+ document.getElementById("analisis").value;
-    var dx="\n\n Diagnostico:"+ document.getElementById("idx").value;
-    var plan="\n\n Plan:"+ document.getElementById("plan").value+"\n";
+    var app = "\n\n App:\n " + document.getElementById("alergia").value + document.getElementById("enfermedades").value + document.getElementById("cirugias").value  + document.getElementById("hospitalizaciones").value  + document.getElementById("traumatismo").value + document.getElementById("transfusion").value;  
+    var apnp = "\n\n Apnp:\n " + document.getElementById("etilismo").value + document.getElementById("tabaquismo").value + document.getElementById("toxico").value ;  
+    var gine = "\n\n Gyo: \n " + "Menarca:" + " " + document.getElementById("menarca").value + " " + "Ivsa:" + document.getElementById("ivsa").value + " " + "Npsa:" + document.getElementById("npsa").value + " " +"Gestas:" + document.getElementById("gesta").value + " " + "Partos:" + document.getElementById("parto").value + " " + "Cesareas:" + document.getElementById("cesarea").value + " " + "Aborto:"+ document.getElementById("aborto").value + " " + "Citologia:" + document.getElementById("citologia").value + " " + "Fum:" + document.getElementById("fum").value;
+    var pa = "\n\n Pa:\n " + document.getElementById("pa").value;
+    var ef="\n\n Ef:\n " + document.getElementById("neuro").value + document.getElementById("piel").value + document.getElementById("cabeza").value + document.getElementById("cuello").value + document.getElementById("torax").value + document.getElementById("abdomen").value + document.getElementById("genitales").value + document.getElementById("extremidad").value;
+    var analisis="\n\n Analisis:\n "+ document.getElementById("analisis").value;
+    var dx="\n\n Diagnostico:\n "+ document.getElementById("idx").value;
+    var plan="\n\n Plan:\n "+ document.getElementById("plan").value+"\n";
     var text= document.getElementById("final");
     
     
     if(document.getElementById("sexo").value == "femenino"){
-        text.value = nombre1 + dato + app + apnp + puta + pa + ef + analisis+dx+plan;
+        text.value = nombre1 + dato + app + apnp + gine + pa + ef + analisis+dx+plan;
     }else{
         text.value = nombre1 + dato + app + apnp + pa + ef + analisis+dx+plan;
     }
@@ -22,20 +31,16 @@ function nota(form){
 
 function indicaciones(form){
 
-
-    if (document.getElementById("alarma").value == "N/a Alarma"){
-        var text= document.getElementById("final");
-        text.value += "Medidas generales:\n"+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia."; 
-     }else if (document.getElementById("alarma").value == "Deshidratacion"){
-        var text= document.getElementById("final");
-        text.value += "Medidas generales:\n"+"Datos de alarma(llanto sin lagrimas, hundimiento de ojos,irritabilidad, boca y lengua seca, llanto sin lagrimas, no presenta micciones)."+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia."; 
-        }else if(document.getElementById("alarma").value == "Respiratoria"){
-        var text= document.getElementById("final");
-        text.value += "Medidas generales:\n"+"Datos de alarma(disnea, sibilancias, piel que se hunde entre costillas, cambio de coloración de unas y labios). "+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia.";
-        }else if(document.getElementById("alarma").value == "Obstetrica"){
-        var text= document.getElementById("final");
+    if (document.getElementById("alarma").value == "Deshidratacion"){
+        var text= document.getElementById("plan");
+        text.value += "Medidas generales:\n"+"Datos de alarma(llanto sin lagrimas, hundimiento de ojos,irritabilidad, boca y lengua seca, no presenta micciones)."+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia."; 
+    }else if(document.getElementById("alarma").value == "Respiratoria"){
+        var text= document.getElementById("plan");
+        text.value += "Medidas generales:\n"+"Datos de alarma(disnea, sibnilancias, piel que se unde entre costillas, cambio de coloracion de unas y labios). "+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia.";
+    }else if(document.getElementById("alarma").value == "Obstetrica"){
+        var text= document.getElementById("plan");
         text.value += "Medidas generales:\n"+"Datos de alarma(Sangrado vaginal,  convulsiones, falta de motilidad fetal, fiebre, dolor de cabeza, ver luces, escuchar zumbidos, dolor abdominal.) "+ "\nCita abierta a urgencias en caso de presentar datos de alarma."+"\nSeguimiento con medico familair en caso de continuar con sintomatologia.";
-        }
+    }
 }
 
 function calc(form){
@@ -89,54 +94,57 @@ function calc(form){
     }
 
 }
+
 function calc2(form){
     
-    var medicamento = document.getElementById("medicamento").value;
-    var A = document.getElementById("peso").value;
-    var B = document.getElementById("mg").value;
-    var C = document.getElementById("ml").value;
-    var D = document.getElementById("dosis").value;
-    var E = document.getElementById("hrs").value;
-    var F = "0";
-    if (A => 40){
-        var L = medicamento +" "+ B + " mg  cada "+ E +"hrs por "+document.getElementById("dias").value+" días";
+    let medicamento ={
+        nombre: document.getElementById("medicamento").value,
+        mg: document.getElementById("mg").value,
+        ml: document.getElementById("ml").value,
+        dosis: document.getElementById("dosis").value,
+        horas: document.getElementById("horario").value,
+        dia:document.getElementById("dias").value
+    }
+    if (document.getElementById("peso").value >= 40){
+        var L = medicamento.nombre +" "+ medicamento.mg + " mg  cada "+ medicamento.horas +"hrs por "+medicamento.dia+" días";
         var text= document.getElementById("final");
         text.value += L+"\n";
         return false;
-    }else{
+    }
+    else{
         if (document.getElementById("dia").checked){
-            var F = (((A * D) * (C) / B)/E);
+            var A = document.getElementById("peso").value;
+            var F = (((A * medicamento.dosis) * (medicamento.ml) / medicamento.mg) / medicamento.horas);
             var F = F.toFixed(2)
-            var L = medicamento +" "+ F + " ml  cada "+ E +"hrs por "+document.getElementById("dias").value+" días";
-            var text= document.getElementById("final");
+            var L = medicamento.nombre + " " +medicamento.mg + " mg/" + medicamento.ml +" ml, tomar "+ F + " ml  cada "+ medicamento.horas +"hrs por "+medicamento.dia+" días";
+            var text= document.getElementById("plan");
             text.value += L+"\n";
             return false;
         }
         else{
-            var F = (((A * D) * C) / B);
+            var A = document.getElementById("peso").value;
+            var F = (((A * medicamento.dosis) * medicamento.ml) / medicamento.mg);
             var F = F.toFixed(2)
-            var L = medicamento + F + " ml  cada "+ E +"hrs por "+document.getElementById("dias").value+" días";
-            var text= document.getElementById("final");
+            var L = medicamento.nombre + " " + medicamento.mg+ " mg/" + medicamento.ml +" ml, tomar "+F+ " ml  cada "+ medicamento.horas +"hrs por "+medicamento.dia+" días";
+            var text= document.getElementById("plan");
             text.value += L+"\n";
             return false;
         }
 
     }
-    
-
 }
 
 function download(){
     var nombre1 = document.getElementById("nombre").value+ document.getElementById("apellido-paterno").value+ document.getElementById("apellido-materno").value;
     var text = document.getElementById("final").value;
-    text = text.replace(/\n/g, "\r\n"); 
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
     var blob = new Blob([text], { type: "text/plain"});
     var anchor = document.createElement("a");
     anchor.download = nombre1+".txt";
     anchor.href = window.URL.createObjectURL(blob);
     anchor.target ="_blank";
-    anchor.style.display = "none"; 
+    anchor.style.display = "none"; // just to be safe!
     document.body.appendChild(anchor);
     anchor.click();
     document.body.removeChild(anchor);
- }
+}
